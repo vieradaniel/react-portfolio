4105fffdd23510101b8afb4456527e15ff4c7c88
vieradaniel <dariennx@hotmail.com>
2022-06-25 12:12:37 -0300

    ternary added on taskbar to display StartMenu

--- src/components/StartMenu/StartMenu.jsx
+++ src/components/StartMenu/StartMenu.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./StartMenu.css";
 
-export const StartMenu = () => {
+export const StartMenu = ({ startButton }) => {
   return (
-    
-      <div className="startMenu-container">
-        <div className='startMenu-container-top'></div>
-        <div className='startMenu-container-bottom'></div>
-      
-      </div>
-    
+    <>
+      {
+      startButton 
+      && 
+      (
+        <div className="startMenu-container">
+          <div className="startMenu-container-top"></div>
+          <div className="startMenu-container-bottom"></div>
+        </div>
+      )
+      }
+    </>
   );
 };
--- src/components/Taskbar/Taskbar.jsx
+++ src/components/Taskbar/Taskbar.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { StartMenu } from '../StartMenu/StartMenu';
-import './Taskbar.css'
+import React, { useState } from "react";
+import { StartMenu } from "../StartMenu/StartMenu";
+import "./Taskbar.css";
 
 export const Taskbar = () => {
+  const [startButton, setStartButton] = useState(false);
 
-  const handleStartClick= ()=>{
-    console.log('hi');
-  }
+  const handleStartClick = () => {
+    setStartButton( startButton ? false : true);
+    
+  };
 
   return (
     <>
-    <div className="Taskbar-Container" onClick={handleStartClick}>
+      <div className="Taskbar-Container" onClick={handleStartClick}>
         <div className="taskbar-icon-container">
-
-            <img className="taskbar-icon" src="../src/assets/icons/start.png" alt="hi" />
-            
+          <img
+            className="taskbar-icon"
+            src="../src/assets/icons/start.png"
+            alt="hi"
+          />
         </div>
-    </div>
-    <StartMenu  />
+      </div>
+      <StartMenu startButton={startButton}/>
     </>
-  )
-}
+  );
+};
